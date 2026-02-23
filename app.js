5ac1ed5b466701ce773ee81ab44584c0512b5cb2
grillex-dev <m.grillex69@gmail.com>
2026-02-24 01:02:28 +0200

    allowing all cors

--- app.js
+++ app.js
@@ -17,15 +17,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://127.0.0.1:3000",
-      process.env.FRONTEND_URL,
-    ].filter(Boolean),
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  })
+  }),
 );
 
 app.get("/debug-env", (req, res) => {
