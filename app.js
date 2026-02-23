1dd6d01b360064fa0d9a178db785f7c7011b90b0
Ahmed-jsx <afathi245@gmail.com>
2026-02-23 23:57:26 +0200

    fix: fix cors error

--- app.js
+++ app.js
@@ -17,8 +17,14 @@ const app = express();
 
 app.use(
   cors({
-    origin: true, // Allow all origins
+    origin: [
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      process.env.FRONTEND_URL,
+    ].filter(Boolean),
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
